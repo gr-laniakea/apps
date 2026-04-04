60de58a82ec81f15c1ce9981b61038d292d8ce80
GregRos <life.gregr@gmail.com>
2026-04-04 22:53:49 +0100

    fix: change urls

--- cluster/svcs/wg-easy/index.ts
+++ cluster/svcs/wg-easy/index.ts
@@ -52,9 +52,10 @@ export default W.File(`${name}.yaml`, {
                                 key: "ADMIN_PASSWORD"
                             },
                             WG_PORTAL_WEB_LISTENING_ADDRESS: `:${webPort}`,
-                            WG_PORTAL_WEB_EXTERNAL_URL: `http://${ipWgClientPortal}`,
+                            WG_PORTAL_WEB_EXTERNAL_URL: `http://wg-client.lk.host`,
                             WG_PORTAL_ADVANCED_START_LISTEN_PORT: udpPort,
                             WG_PORTAL_ADVANCED_USE_IP_V6: "false",
+                            WG_CLIENT_DASHBOARD_SERVICE_HOST: "wg-client.lk.host",
                             WG_PORTAL_ADVANCED_CONFIG_STORAGE_PATH: "/etc/wireguard"
                         },
                         $resources: {
